11f5eacc8169da4a20d3945248a5c03db3aafa07
buckmelton <buck9063@gmail.com>
2018-01-04 14:34:57 -0800

    Create and hook up first field of form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends React.Component {
+
+  renderTitleField(field) {
+    return (
+      <div>
+        <input
+          type="text"
+          {...field.input}
+          // above replaces
+          // onChange={field.input.onChange}
+          // onFocus={field.input.onFocus}
+          // onBlur={field.input.onBlur}
+        />
+      </div>
+    );
+  }
+
   render() {
     return (
       <div>
-        PostsNew!
+        <form>
+          <Field
+            name="title"
+            component={this.renderTitleField}
+          />
+        </form>
       </div>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
