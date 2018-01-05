f5c2f3765fca247445098f003040120c6ef66562
buckmelton <buck9063@gmail.com>
2018-01-05 13:16:30 -0800

    Add post content form field

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,10 +3,10 @@ import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends React.Component {
 
-  renderTitleField(field) {
+  renderField(field) {
     return (
       <div className="form-group">
-        <label>Title</label>
+        <label>{field.label}</label>
         <input
           className="form-control"
           type="text"
@@ -21,8 +21,19 @@ class PostsNew extends React.Component {
       <div>
         <form>
           <Field
+            label="Title"
             name="title"
-            component={this.renderTitleField}
+            component={this.renderField}
+          />
+          <Field
+            label="Tags"
+            name="tags"
+            component={this.renderField}
+          />
+          <Field
+            label="Post Content"
+            name="content"
+            component={this.renderField}
           />
         </form>
       </div>
