5c24207fde81f2ed1c06b248259b0353e1e50e9d
buckmelton <buck9063@gmail.com>
2018-01-08 11:59:47 -0800

    Add action and reducer for 'fetch post'

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,16 @@
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 import _ from 'lodash';
 
 export default function(state = {}, action) {
   switch (action.type) {
+  case FETCH_POST:
+    // const post = action.payload.data;
+    // const newState = { ...state };
+    // newState[post.id] = post;
+    // return newState;
+
+    // The following ES6 line is equivalent the the 4 ES5 lines above.
+    return { ...state, [action.payload.data.id]: action.payload.data };
   case FETCH_POSTS:
     return _.mapKeys(action.payload.data, 'id');
   default:
