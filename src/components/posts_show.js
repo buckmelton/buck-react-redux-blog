22e8a5027ec2c72adefce557c52b40275f04130c
buckmelton <buck9063@gmail.com>
2018-01-12 13:10:10 -0800

    Add post delete button

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -9,6 +9,10 @@ class PostsShow extends Component {
     this.props.fetchPost(id);
   }
 
+  onDeleteClick(event) {
+    
+  }
+
   render() {
     const { post } = this.props;
 
@@ -18,7 +22,12 @@ class PostsShow extends Component {
 
     return (
       <div>
-        <Link to="/" >Back To Index</Link>
+        <Link to="/" className="btn btn-primary">Back To Index</Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}>
+          Delete Posts
+        </button>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
